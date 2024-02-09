0400e5f3c8677757c9c4a27a5f524244d1df10b2
Esther White <monacodelisa@gmail.com>
2024-02-09 10:06:16 +0200

    json sorrected to give boolean status not string ;)

--- angular-dashboard/src/app/components/server/server.component.ts
+++ angular-dashboard/src/app/components/server/server.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Server } from 'src/app/models/server';
 
 @Component({
@@ -6,7 +6,30 @@ import { Server } from 'src/app/models/server';
   templateUrl: './server.component.html',
   styleUrls: ['./server.component.scss']
 })
-export class ServerComponent {
+export class ServerComponent implements OnInit {
+  serverStatus!: boolean;
+  severity: string = '';
+  buttonText: string = '';
+
   @Input() serverInput!: Server;
 
+  ngOnInit(): void {
+    this.setServerStatus(this.serverInput.isOnline);
+  }
+
+  setServerStatus(isOnline: boolean) {
+    if (isOnline) {
+      this.serverInput.isOnline = true;
+      this.severity = 'success';
+      this.buttonText = 'Shut Down';
+    } else {
+      this.serverInput.isOnline = false;
+      this.severity = 'danger';
+      this.buttonText = 'Start';
+    }
+  }
+
+  toggleStatus(onlineStatus: boolean) {
+    this.setServerStatus(!onlineStatus);
+  }
 }
