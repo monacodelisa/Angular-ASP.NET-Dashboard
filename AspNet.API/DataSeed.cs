455add3fd6c2f97ddcd0cbeda6273d051af59299
Esther White <monacodelisa@gmail.com>
2024-02-09 19:53:16 +0200

    list of servers

--- AspNet.API/DataSeed.cs
+++ AspNet.API/DataSeed.cs
@@ -9,11 +9,11 @@ namespace AspNet.API
         public DataSeed(ApiContext ctx)
         {
             _ctx = ctx;
-        }  
-        
+        }
+
         public void SeedData(int nCustomers, int nOrders)
         {
-            if (!_ctx.Customers.Any()) 
+            if (!_ctx.Customers.Any())
             {
                 SeedCustomers(nCustomers);
             }
@@ -25,29 +25,39 @@ namespace AspNet.API
 
             if (!_ctx.Servers.Any())
             {
-                SeedServers(nServers);
+                SeedServers();
             }
 
             _ctx.SaveChanges();
         }
 
-        private void SeedCustomers (int n)
+        private void SeedCustomers(int n)
         {
             List<Customer> customers = BuildCustomerList(n);
 
-            foreach (Customer customer in customers) 
-            { 
+            foreach (Customer customer in customers)
+            {
                 _ctx.Customers.Add(customer);
             }
         }
 
-        private void SeedOrders (int n) 
+        private void SeedOrders(int n)
         {
             List<Order> orders = BuildOrderList(n);
 
             foreach (Order order in orders)
             {
                 _ctx.Orders.Add(order);
+            }
+        }
+
+        private void SeedServers()
+        {
+            List<Server> servers = BuildServerList();
+
+            foreach (var server in servers)
+            {
+                _ctx.Servers.Add(server);
             }
         }
 
@@ -95,6 +105,23 @@ namespace AspNet.API
             }
 
             return orders;
+        }
+
+        private List<Server> BuildServerList()
+        {
+            return new List<Server>()
+            {
+                new Server { Id = 1, Name = "Dev-Web", IsOnline = true },
+                new Server { Id = 2, Name = "Test-DB", IsOnline = true },
+                new Server { Id = 3, Name = "Prod-App", IsOnline = false },
+                new Server { Id = 4, Name = "Staging-Web", IsOnline = true },
+                new Server { Id = 5, Name = "Backup-DB", IsOnline = false },
+                new Server { Id = 6, Name = "QA-App", IsOnline = true },
+                new Server { Id = 7, Name = "Demo-Web", IsOnline = false },
+                new Server { Id = 8, Name = "Backup-App", IsOnline = true },
+                new Server { Id = 9, Name = "Dev-DB", IsOnline = false }
+
+            };
         }
     }
 }
