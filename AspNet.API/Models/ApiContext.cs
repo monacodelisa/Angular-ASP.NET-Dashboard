bc5950c095ffc46c6d5165196e60cf1830d082e0
Esther White <monacodelisa@gmail.com>
2024-02-09 11:04:55 +0200

    dbcontext

--- AspNet.API/Models/ApiContext.cs
+++ AspNet.API/Models/ApiContext.cs
@@ -2,7 +2,11 @@
 
 namespace AspNet.API.Models
 {
-    public class ApiContext
+    public class ApiContext : DbContext
     {
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options) { }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Server> Servers { get; set; }
     }
 }
