0b98d71edc23ff450f00d7d42bcfb11325124592
Esther White <monacodelisa@gmail.com>
2024-02-09 15:07:50 +0200

    working on setting up the db

--- AspNet.API/Program.cs
+++ AspNet.API/Program.cs
@@ -1,11 +1,21 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore; 
+using AspNet.API.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApiContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Host=[DB SERVER URL]; Database=[DB NAME]; Username=[USERNAME];Password=[PASSWORD]")));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
 
 var app = builder.Build();
 
